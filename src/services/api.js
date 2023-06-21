86a6b15a89a4c2201e5196cd879dc969fc96e4a1
lucas-brilhante <lucasbrilhantepires@gmail.com>
2023-06-21 09:21:54 -0300

    feat: atualiza rota da api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Get Product Catalog from API
 
 const api = async () => {
-  const response = await axios.get('https://undefined.netlify.app/api/catalog')
+  const response = await axios.get('https://5f184aca7c06c900160dcd19.mockapi.io/api/v1/catalog')
   return response.data
 }
 export default api
